dda46f0f658ce802947bf7159afe4a711446749e
Fabrizio Gomez Ruiz <fabrigoru@gmail.com>
2026-01-20 16:58:49 -0600

    Nativewind base

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,15 +1,10 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+
+const { withNativeWind } = require('nativewind/metro');
+
+/** @type {import('expo/metro-config').MetroConfig} */
 
 const config = getDefaultConfig(__dirname);
 
-config.server = {
-  ...config.server,
-  enhanceMiddleware: (middleware) => {
-    return (req, res, next) => {
-
-      return middleware(req, res, next);
-    };
-  },
-};
-
-module.exports = config;
+module.exports = withNativeWind(config, { input: './global.css' });
