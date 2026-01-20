b14588a074feb0aabeff5fdfd3fcdf573c50ba83
Fabrizio Gomez Ruiz <fabrigoru@gmail.com>
2026-01-20 17:15:23 -0600

    ESLint config tweak

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,14 +1,22 @@
 /* eslint-env node */
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const reactHooks = require('eslint-plugin-react-hooks')
+const tanstackEslint = require("@tanstack/eslint-plugin-query")
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', "node_modules/*"],
   },
   {
+
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+            "@tanstack/query": tanstackEslint,
+    },
     rules: {
+      ...reactHooks.configs.recommended.rules,
       'react/display-name': 'off',
     },
   },
